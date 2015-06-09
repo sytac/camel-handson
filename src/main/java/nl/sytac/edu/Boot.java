741823a1d45a5574579f831ea054155058ed9c9b
pierangelo <pierangeloc@gmail.com>
2015-06-09 23:07:40 +0200

    minor adjustment

--- src/main/java/nl/sytac/edu/Boot.java
+++ src/main/java/nl/sytac/edu/Boot.java
@@ -1,17 +1,17 @@
 package nl.sytac.edu;
 
 import nl.sytac.edu.components.routes.SimpleRouteBuilder;
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.SpringCamelContext;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.apache.camel.spring.javaconfig.Main;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * We extend CamelConfiguration so we can build a Camel Context using purely annotated classes
  *
  */
+@Configuration
 public class Boot extends CamelConfiguration {
 
     public static void main( String[] args ) throws Exception {
@@ -21,13 +21,9 @@ public class Boot extends CamelConfiguration {
         main.setBasedPackages("nl.sytac.edu.components");
 
         main.run();
-        System.out.println("ciao");
+
     }
 
-    @Override
-    protected CamelContext createCamelContext() throws Exception {
-        return new SpringCamelContext(getApplicationContext());
-    }
 
     @Bean
     public RouteBuilder getSimpleRouteBuilder() {
