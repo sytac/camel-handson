8703dd843cc8ac312b8af8dfd886e63bed09da99
pierangelo <pierangeloc@gmail.com>
2015-07-07 00:04:24 +0200

    simple rest API exposed, running

--- src/main/java/nl/sytac/edu/Boot.java
+++ src/main/java/nl/sytac/edu/Boot.java
@@ -3,19 +3,20 @@ package nl.sytac.edu;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.apache.camel.spring.javaconfig.Main;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * We extend CamelConfiguration so we can build a Camel Context using purely annotated classes
  *
  */
 @Configuration
+@ImportResource("classpath:spring/spring.xml")
 public class Boot extends CamelConfiguration {
 
     public static void main( String[] args ) throws Exception {
 
         Main main = new Main();
         //use any route builder and components declared within this package
-        main.setApplicationContextUri("/spring/spring.xml");
         main.setBasedPackages("nl.sytac.edu");
 
         main.run();
