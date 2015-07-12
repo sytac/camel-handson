ec3b524e495b630154a5cbbc6ce89d8a7d39edee
pierangelo <pierangeloc@gmail.com>
2015-07-12 21:58:49 +0200

    with file persistence

--- src/main/java/io/sytac/edu/routes/SimpleRouteBuilder.java
+++ src/main/java/io/sytac/edu/routes/SimpleRouteBuilder.java
@@ -12,6 +12,7 @@ public class SimpleRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
         from("cxfrs:bean:restInventory")
             .routeId("restInventoryRoute")
+            .to("file:///tmp/inventory?fileName=inventory-${date:now:yyyyMMdd@HHmmssSS}.json")
             .setBody(constant(new Response("Ok", "stock update received")));
     }
 }
