a8ab1b8b053103e25573bed818b8032c08d2f813
Pierangelo Cecchetto <pierangeloc@gmail.com>
2015-07-23 09:49:24 +0200

    extracted constants for endpoints

--- src/main/java/io/sytac/edu/routes/SimpleRouteBuilder.java
+++ src/main/java/io/sytac/edu/routes/SimpleRouteBuilder.java
@@ -8,17 +8,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class SimpleRouteBuilder extends RouteBuilder {
 
+    public static final String FILE_INVENTORY_ENDPOINT = "file:///tmp/inventory?fileName=inventory-${date:now:yyyyMMdd@HHmmssSS}.json";
+    public static final String MONGO_INVENTORY_ENDPOINT = "mongodb:mongoClient?database=inventory&collection=updates&operation=save";
+    public static final String FILE_ERROR_ENDPOINT = "file:///tmp/inventory?fileName=error-${date:now:yyyyMMdd@HHmmssSS}.json";
+
     @Override
     public void configure() throws Exception {
         from("cxfrs:bean:restInventory")
             .routeId("restInventoryRoute")
                 .choice()
                     .when().jsonpath("$[?(@.shoes)]")
-                        .to("file:///tmp/inventory?fileName=inventory-${date:now:yyyyMMdd@HHmmssSS}.json")
+                        .to(FILE_INVENTORY_ENDPOINT)
                     .when().jsonpath("$[?(@.jeans)]")
-                        .to("mongodb:mongoClient?database=inventory&collection=updates&operation=save")
+                        .to(MONGO_INVENTORY_ENDPOINT)
                     .otherwise()
-                        .to("file:///tmp/inventory?fileName=error-${date:now:yyyyMMdd@HHmmssSS}.json")
+                        .to(FILE_ERROR_ENDPOINT)
                 .end()
             .setBody(constant(new Response("Ok", "stock update received")));
     }
